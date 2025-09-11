c72c3cd1f1f67fefa29935e3b80f68a4cbfdebc2
kaleokromero <kaleo.romero@gmail.com>
2025-09-11 07:51:22 -0300

    refactor: remoção de getters e setters manuais em entity

--- src/main/java/com/meisters/task_manager/entity/Tasks.java
+++ src/main/java/com/meisters/task_manager/entity/Tasks.java
@@ -9,7 +9,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Getter
+@Setter
 @Table(name = "tasks")
 public class Tasks {
     
@@ -34,56 +39,4 @@ public class Tasks {
 
     @Column(name = "status")
     private status status;
-
-    public Tasks(){}
-
-    public Tasks(String name, String description, LocalDate dueDate,
-            com.meisters.task_manager.entity.Tasks.status status) {
-        this.name = name;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.status = status;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(LocalDate dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public status getStatus() {
-        return status;
-    }
-
-    public void setStatus(status status) {
-        this.status = status;
-    }
-
-    
 }
